0d184af1bf2dd070205c700849d9cfc880309df2
ltaverni <>
2026-03-19 15:42:17 +0100

    (feat) create app skeletton

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,70 @@
 import './App.css'
 
-function App() {
+const Links = () => {
     return (
-        <div>IT WORKS</div>
+        <div className='links'>
+            <div className='link-container'>
+                <a>GitHub</a>
+            </div>
+            <div className='link-container'>
+                <a>Frontend Mentor</a>
+            </div>
+            <div className='link-container'>
+                <a>Linkedin</a>
+            </div>
+            <div className='link-container'>
+                <a>Twitter</a>
+            </div>
+            <div className='link-container'>
+                <a>Instagram</a>
+            </div>
+        </div>
+    )
+}
+
+const Description = () => {
+    return (
+        <div className='description'>"Front-end developer and avid reader"</div>
+    )
+}
+
+const Address = () => {
+    return (
+        <div className='address'>London, United Kingdom</div>
+    )
+}
+
+const Name = () => {
+    return (
+        <div className='name'>Jessica Randall</div>
+    )
+}
+
+const Picture = () => {
+    return (
+        <div className='picture'>
+            <img></img>
+        </div>
+    )
+}
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <Picture />
+            <Name />
+            <Address />
+            <Description />
+            <Links />
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <main>
+            <Card />
+        </main>
     )
 }
 
